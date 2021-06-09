2d677dc064b003a935cae948c5a669c5964dafe2
Mathilde <mathilde.vigouroux.simplon@gmail.com>
2021-06-09 11:36:52 +0200

    make ball moove

--- script.js
+++ script.js
@@ -30,10 +30,15 @@ box.style.border = "solid 2px #bbb44f";
 box.style.borderRadius = "5px";
 screen.appendChild(box);
 
+
+//position
+let positionBottom = 12;
+let positionLeft = 25;
+
 const perso = document.createElement('div');
 perso.style.position = "absolute";
-perso.style.bottom = "12px";
-perso.style.left = "25px";
+perso.style.bottom = `${positionBottom}px`;
+perso.style.left = `${positionLeft}px`;
 perso.style.width = "30px";
 perso.style.height = "30px";
 perso.style.backgroundColor = "red";
@@ -43,5 +48,123 @@ screen.appendChild(perso);
 
 root.appendChild(screen);
 
+//create classes
+// class Perso {
+//     constructor(positionLeft, positionBottom) {
+//         this.positionLeft = positionLeft;
+//         this.positionBottom = positionBottom;
+//     }
+
+//     get moveInFront() {
+//         return this.positionLeft += 100
+//     }
+
+//     get moveBack() {
+//         return this.positionLeft -= 100
+//     }
+
+//     get jump() {
+
+//     }
+// }
+
+// class Position extends Perso {
+
+//     constructor(positionLeft, positionBottom) {
+//         super(positionLeft, positionBottom)
+//     }
+
+//     get initPositionLeft() {
+//         return this.positionLeft = 25
+//     }
+
+//     get initPositionBottom() {
+//         return this.positionBottom = 12
+//     }
+
+//     get onTheBoxPositionLeft() {
+//         return this.positionLeft = 595
+//     }
+
+//     get onTheBoxPositionBottom() {
+//         return this.positionBottom = 134
+//     }
+
+// }
+
+//const initPosition = new Position(25,12);
+
+//console.log(Position.initPositionLeft);
+
+
 
 // Make the character move and jump!
+
+//check a keydown event on arrowRight, Up, Left
+document.addEventListener('keydown',(e) => {
+    const key = e.keyCode
+
+    switch (key) {
+        case 37 :
+            moveBack();
+            perso.style.left = `${positionLeft}px`;
+            break;
+        case 38 :
+            jump();
+            perso.style.bottom = `${positionBottom}px`;
+            break;
+        case 39 :
+            moveInFront();
+            perso.style.left = `${positionLeft}px`;
+            break;
+        default :
+            break;
+    }
+})
+
+
+
+//move in front of
+function moveInFront(){
+
+    perso.animate([
+        { // from
+            left: `${positionLeft}px`,
+        },
+        { // to
+            left: `${positionLeft += 100}px`
+        }
+    ], 1000);
+}
+
+//move back 
+function moveBack(){
+
+    perso.animate([
+        { left: `${positionLeft}px`},
+        { left: `${positionLeft -= 100}px`}
+    ], 1000);
+}
+
+
+//jump 
+function jump(){
+
+    //next to the box
+    if (positionLeft >= 575 ) {
+
+        perso.animate([
+            { bottom: `${positionBottom}px` },
+            { bottom: `${positionBottom + 150}px` },
+            { bottom: `${positionBottom = 134}px` }
+        ], 500);
+    }
+    else {
+        
+        perso.animate([
+            { bottom: `${positionBottom}px`},
+            { bottom: `${positionBottom + 150}px`},
+            { bottom: `${positionBottom = 12}px`},
+        ], 500);
+    }
+}
